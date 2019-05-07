c4c0d3debd6e6e198eae1f359cb76721480d3e8f
Eunchurn Park <eunchurn.park@gmail.com>
2019-05-07 17:06:31 +0900

    init commit noise data

---
+++ genNoise.m
@@ -0,0 +1,19 @@
+clear all; clc; close all;
+
+Fs=192000;
+
+%%
+% Generating white noise
+duration = 4; % second
+nextP2 = nextpow2(Fs*duration);
+Nsample = 2^nextP2;
+seedX = randn(Nsample,1);
+normX = seedX/max(abs(seedX));
+
+audiowrite('white_noise_5.46s_192kHz_32bit_stereo.wav',[normX, normX],Fs,'BitsPerSample',32);
+audiowrite('white_noise_5.46s_192kHz_32bit_mono.wav',normX,Fs,'BitsPerSample',32)
+
+%%
+% Generating interval white noise
+duration = 2; % second
+release = 0.5; % second
